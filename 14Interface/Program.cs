7f99f9971821245eed1a17706e79f5edd9defa39
Guilherme Stein Zanollo <guilherme.zanollo@db1.com.br>
2025-01-27 15:34:22 -0300

    Última atualização do curso de de POO

--- 14Interface/Program.cs
+++ 14Interface/Program.cs
@@ -10,13 +10,18 @@ string texto();
 void mensagem(string nome, int idade);
 No C# por padrão um método criado na Interface é abstrato e público;
 Nas boas práticas do C#, toda a interface tem a inicial 1, em seguida o nome da interface:
-|Padrao
-|Calculo
-|Consulta
-|Acoes
+IPadrao
+ICalculo
+IConsulta
+IAcoes
 
 */
 class Program
 {
-    static void Main ([])
+    static void Main (string[] args)
+    {
+        Calculo c = new Calculo();
+        c.somar(10, 15);
+        c.subtrair(50, 18);
+    }
 }
